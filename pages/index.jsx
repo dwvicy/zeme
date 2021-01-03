9054574dc27d18f8fec6472031f63ff5ff680b60
Vaishnavi <vaishdw2207@gmail.com>
2021-01-03 23:11:27 +0530

    chores

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,39 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import Link from 'next/link'
+import { useColorMode } from 'theme-ui'
 
-export default () => (
+export default props => {
+  const [colorMode, setColorMode] = useColorMode()
+  return (
   <div sx={{ height: `calc(100vh - 60px)`}}>
+        <header>
+  <button
+    onClick={e => {
+      setColorMode(colorMode === 'default' ? 'dark' : 'default')
+    }}>
+    Toggle {colorMode === 'default' ? 'Dark' : 'Light'}
+  </button>
+</header>
     <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
       <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+  
     </div>
+
   </div> 
+ 
 )
+}
+<style jsx global>{`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: Poppins;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`}</style>
+  
--- theme.js
+++ theme.js
@@ -1,12 +1,28 @@
 import {roboto} from '@theme-ui/presets';
+import {funk} from '@theme-ui/presets';
+
+
 
 const theme = {
-  ...roboto,
+  
+  ...funk,
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: '#0cf',
+      }
+    }
+  },
+  
   containers: {
     card: {
       boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
       border: '1px solid',
-      borderColor: 'muted',
       borderRadius: '4px',
       p: 2,
     },
@@ -17,9 +33,12 @@ const theme = {
       mx: 'auto',
     },
   },
-  styles: {
-    ...roboto.styles,
-  },
+
+    
+  style: {
+    ...funk.export,
+  }
+
 };
 
 export default theme;
