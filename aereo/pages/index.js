df487b40b8fc5ef2ed9e238083bf7192a6db70a7
Vaishnavi <vaishdw2207@gmail.com>
2021-01-28 12:18:38 +0530

    refactor: some changes

--- aereo/pages/index.js
+++ aereo/pages/index.js
@@ -1,34 +1,30 @@
-
 import Nav from '../src/components/nav'
 import Image from 'next/image'
 import Buttonz from '../src/components/button'
-
 
 export default function IndexPage() {
   return (
     <div>
       <div className="py-20">
         <h1 className="text-5xl text-center text-black dark:text-white font-black">
-            The planet doesn't need saving,
-            <span className="text-5xl text-center text-white dark:text-white font-black">
-                you do!
-            </span></h1>
+          The planet doesn't need saving,
+          <span className="text-5xl text-center text-white dark:text-white font-black">
+            you do!
+          </span>
+        </h1>
         <h2 className="py-4 text-2xl text-center font-light dark:text-white">
           Climate action is the best insurance plan for your future
         </h2>
         <div className="flex items-center justify-center p-8 space-x-4">
-            <Buttonz text="Take Action" onClick="/"/>
-          <Buttonz text="Tell me more" onClick="/"/>
+          <Buttonz text="Take Action" onClick="/action" />
+          <Buttonz text="Tell me more" onClick="/about" />
         </div>
       </div>
-      
-
-      </div>
-      // {/* <Image
-      //   src="https://i.imgur.com/3hpncHZ.jpg"
-      //   alt=""
-      //   layout='fill'
-      // /> */}
-   
+    </div>
+    // {/* <Image
+    //   src="https://i.imgur.com/3hpncHZ.jpg"
+    //   alt=""
+    //   layout='fill'
+    // /> */}
   )
 }
--- aereo/src/components/nav.js
+++ aereo/src/components/nav.js
@@ -1,12 +1,9 @@
-import Link from 'next/link';
-
-
-
+import Link from 'next/link'
 
 const links = [
-  { href: '/about', label: 'about'},
-  { href: '/tracker', label: 'tracker'},
-  { href: '/login', label: 'login'},
+  { href: '/about', label: 'about' },
+  { href: '/tracker', label: 'tracker' },
+  { href: '/action', label: 'action' },
 ]
 
 export default function Nav() {
@@ -16,27 +13,21 @@ export default function Nav() {
         <li>
           <Link href="/">
             <a className="text-white no-underline text-accent-1 text-lg font-bold dark:text-white ">
-            <div className=" border border-white">
-              <p className="px-4 py-2 text-white font-bold">
-                  aereor
-              </p>
-            </div>
+              <div className=" border border-white">
+                <p className="px-4 py-2 text-white font-bold">aereor</p>
+              </div>
             </a>
           </Link>
         </li>
-        <div className=" border border-white shadow-lg">
-        <ul className="px-4 py-2 flex items-center justify-between space-x-4">
-          {links.map(({ href, label}) => (
-            <li key={`${href}${label}`}>
-              <Link href={href}>
-                 {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <div className="text-white border border-white shadow-lg">
+          <ul className="px-4 py-2 flex items-center justify-between space-x-4">
+            {links.map(({ href, label }) => (
+              <li key={`${href}${label}`}>
+                <Link href={href}>{label}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
-       
-       
       </ul>
     </nav>
   )
